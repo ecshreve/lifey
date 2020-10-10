978109dc870180c2ba17e0057fd2f90b2df73c72
Eric Shreve <ecshreve13@gmail.com>
2020-10-10 14:57:34 -0700

    grid: add basic update functions and Tick function

--- cmd/lifey/main.go
+++ cmd/lifey/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ecshreve/lifey/internal/grid"
 )
 
 func main() {
 	g := grid.NewGrid(3)
 	g.Cells[0][0].Alive = true
-
+	g.Cells[0][1].Alive = true
+	g.Cells[0][2].Alive = true
+	g.Cells[2][2].Alive = true
 	g.PrintGrid()
-	for _, row := range g.Cells {
-		for _, c := range row {
-			fmt.Printf("r: %d, c: %d -- neighbors: %d\n", c.Row, c.Col, len(c.Neighbors))
-		}
-	}
+	g.Tick()
+	g.Tick()
+	g.Tick()
+
+	// g.PrintGrid()
+	// for _, row := range g.Cells {
+	// 	for _, c := range row {
+	// 		fmt.Printf("cur: %v, next: %v -- r: %d, c: %d -- neighbors: %d\n", c.Alive, c.NextState, c.Row, c.Col, len(c.Neighbors))
+	// 	}
+	// }
+	// fmt.Println("---")
+	// g.SetNextState()
+
+	// for _, row := range g.Cells {
+	// 	for _, c := range row {
+	// 		fmt.Printf("cur: %v, next: %v -- r: %d, c: %d -- neighbors: %d\n", c.Alive, c.NextState, c.Row, c.Col, len(c.Neighbors))
+	// 	}
+	// }
 }
